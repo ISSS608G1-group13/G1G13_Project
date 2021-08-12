456e0f87f4b2718613b608c83bbe7bdefb57fb23
liyuhong0110 <yuhong.li.2020@mitb.smu.edu.sg>
2021-08-13 00:24:40 +0800

    1

--- Project_Application/app.R
+++ Project_Application/app.R
@@ -5,35 +5,11 @@ library(tmap)
 library(tidyverse)
 library(rsconnect)
 library(ggplot2)
+library(igraph)
+library(plotly)
 
-# Import dataset
-location_frequency <- read_csv("./data/location frequency.csv")
-
-# Module for popular locations
-histogramUI <- function(id) {
-    tagList(
-        checkboxGroupInput(
-            NS(id,"cardtype"),
-            label = "Popular locations based on:",
-            choices = c("Credit Card"="num_credit",
-                        "Loyalty Card"="num_loyalty"),
-            selected = "num_credit"
-            ),
-        plotOutput(NS(id,"hist"))
-    )
-}
-
-histogramServer <- function(id) {
-    moduleServer(id, function(input, output, session) {
-        output$hist <- renderPlot({
-            ggplot(location_frequency, 
-                   aes(input$location,input$num_credit)) +
-                geom_bar(bins = 20,
-                               color="black",
-                               fill="light blue")
-        }, res = 96)
-    })
-}
+# Import dataset 
+#location_num <- read.csv("./data/location num.csv/")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -46,22 +22,36 @@ ui <- fluidPage(
                    tabPanel("Popular Locations", 
                             sidebarLayout(
                                 sidebarPanel(
-                                    histogramUI()  
+                                    radioButtons(
+                                        inputId = "location_fre",
+                                        label = "Popular locations based on:",
+                                        choices = c("Credit Card"="num_credit",
+                                                    "Loyalty Card"="num_loyalty"),
+                                        selected = "num_credit"
+                                        ),
+                                    helpText("Data from AT&T (1961) The World's Telephones.")
+                                    ),
+                                mainPanel(
+                                    plotlyOutput("LocationPlot")
+                                    )
                                 )),
-                                mainPanel("output")
-                            )),
                    tabPanel("Hotpoints of Social News", 
                             sidebarLayout(
-                                sidebarPanel("input"),
-                                mainPanel("output")))),
-        tabPanel("panel 1", "one"),
-        tabPanel("panel 2", "two"),
-        tabPanel("panel 3", "three")
-        )
+                                sidebarPanel("input","one"),
+                                mainPanel("output","one")))),
+        tabPanel("Consumption Analysis", "one"),
+        tabPanel("Path Visualization", "two"),
+        tabPanel("Network","three")
+        ))
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-    histogramServer("hist")
+    output$LocationPlot <- renderPlotly({
+        ggplot(data = location_num,
+               aes(x=location,y=input$location_fre))+
+            geom_point()
+    })
 }
 
 # Run the application 
